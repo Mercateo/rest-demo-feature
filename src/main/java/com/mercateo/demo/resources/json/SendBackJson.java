97cab8155ac311b329c5e2b53f000e8bd090cdbb
joerg.adler <joerg.adler@mercateo.com>
2016-09-13 11:41:38 +0200

    bugfix constructor

--- src/main/java/com/mercateo/demo/resources/json/SendBackJson.java
+++ src/main/java/com/mercateo/demo/resources/json/SendBackJson.java
@@ -5,10 +5,12 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mercateo.demo.feature.Feature;
 import com.mercateo.demo.feature.KnownFeatureId;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -20,4 +22,9 @@ public class SendBackJson {
 
 	@Feature(KnownFeatureId.TICKET_6)
 	private CARRIER preferredCarrier;
+
+	public SendBackJson(@JsonProperty("message") @NonNull String message) {
+		super();
+		this.message = message;
+	}
 }
